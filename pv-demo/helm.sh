59172180a47e6e53be49afc6691ac6c6eb992b82
Kingdon Barrett <kingdon@weave.works>
2022-04-24 20:02:05 -0400

    just use helm to debug it
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- pv-demo/helm.sh
+++ pv-demo/helm.sh
@@ -6,4 +6,11 @@
 # flux create helmrelease wordpress --export --source HelmRepository/bitnami --namespace default --chart wordpress --export
 # flux create source helm --url https://charts.bitnami.com/bitnami bitnami --export --namespace default
 
+# flux create helmrelease wordpress --export --source HelmRepository/groundhog2k --namespace default --chart wordpress --export
+# flux create source helm --url https://groundhog2k.github.io/helm-charts/ groundhog2k --export --namespace default
+
+# helm repo add groundhog2k https://groundhog2k.github.io/helm-charts/
+# helm fetch --untar groundhog2k/wordpress
+# helm install my-release groundhog2k/wordpress --values wordpress-values.yaml
+
 kubectl apply -f helm-wordpress.yaml
